8d117c22fad67763754348ddcb7932ef427c4add
krkettle <catrarazou@gmail.com>
2021-03-21 23:24:59 +0900

    [update] generate iam_policy by initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -14,7 +14,22 @@ BUCKET_NAME="${PROJECT_NAME}-${AWS_ACCOUNT_ID}"
 aws s3 mb "s3://${BUCKET_NAME}"
 aws s3api put-public-access-block --bucket $BUCKET_NAME --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
 
-#terraform.tfvarsを作成
+# 研修用EC2インスタンス用のIAMポリシーを作成
+# ※研修者リストをS3経由で取得するため
+cat <<EOS >data/iam_policy/MyAmazonS3ReadOnlyAccess.json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["s3:Get*", "s3:List*"],
+      "Resource": "arn:aws:s3:::${BUCKET_NAME}/users/*"
+    }
+  ]
+}
+EOS
+
+# terraform.tfvarsを作成
 cat <<EOS >terraform.tfvars
 project = "${PROJECT_NAME}"
 source_ip_list = [
@@ -22,7 +37,7 @@ source_ip_list = [
 ]
 EOS
 
-#terraform.backend
+# terraform.backend
 cat <<EOS >backend.tfvars
 bucket  = "${BUCKET_NAME}"
 key     = "terraform.tfstate"
