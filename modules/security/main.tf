6337b9b728511df6260f60d96ed75ae3b5d63d67
krkettle <catrarazou@gmail.com>
2021-03-22 01:02:13 +0900

    [remove] http ingress

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -11,15 +11,6 @@ resource "aws_security_group" "tr_sg" {
   }
 }
 
-resource "aws_security_group_rule" "tr_in_http" {
-  security_group_id = aws_security_group.tr_sg.id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 80
-  to_port           = 80
-  cidr_blocks       = var.source_ip_list
-}
-
 resource "aws_security_group_rule" "tr_in_ssh" {
   security_group_id = aws_security_group.tr_sg.id
   type              = "ingress"
